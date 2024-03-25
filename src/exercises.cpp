8f374720bf7a6e91da3e158d9d4db9893aa8bbca
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-03-25 11:35:21 -0400

    Prueba 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -16,3 +16,65 @@ void exercise_1(string s1, string s2, string s3) {
 
 }
 
+void exercise_2(double A, double B, double C) {
+  
+}
+
+void exercise_3(int a, int b) {
+  
+}
+
+void exercise_4(double n, double a, double b, double x, double y) {
+  
+  
+}
+
+void exercise_5(char character) {
+
+}
+
+void exercise_6(int number) {
+ 
+  {
+    
+  }
+}
+
+void exercise_7(double r) {
+  
+}
+
+void exercise_8(long int seconds) {
+  // TODO: YOUR CODE HERE
+}
+
+string exercise_9(string s1, string s2, string s3, string s4, string s5) {
+  
+}
+  
+
+int exercise_10(int a, int b) {
+  // TODO: YOUR CODE HERE
+}
+
+string exercise_11(int number) {
+  
+}
+
+void exercise_12(string color1, int numb1,
+                 string color2, int numb2,
+                 string color3, int numb3,
+                 string color4, int numb4) {
+  // TODO: YOUR CODE HERE
+}
+
+string exercise_13(int age, int years_of_experience) 
+
+string exercise_14(int number_of_docs)
+
+void exercise_15(int a, int b, int c) {
+
+
+void exercise_16(int debut, int fin) {
+  // TODO: YOUR CODE HERE
+}
