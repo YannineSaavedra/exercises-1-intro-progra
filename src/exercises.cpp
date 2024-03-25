7dec11af32718c74bcff81f4b45ae0de50d31faa
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-03-25 12:28:24 -0400

    Prueba 14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -88,8 +88,37 @@ void exercise_5(char character) {
 
 
 void exercise_6(int number) {
-
+cin >> number;
+  switch (number)
+  {
+    case 1:
+    cout << "Lunes";
+    break;
+    case 2:
+    cout << "Martes";
+    break;
+    case 3:
+    cout << "Miercoles";
+    break;
+    case 4:
+    cout << "Jueves";
+    break;
+    case 5:
+    cout << "Viernes";
+    break;
+    case 6:
+    cout << "Sabado";
+    break;
+    case 7:
+    cout << "Domingo";
+    break;
+    default:
+    cout << "invalid input";
+    break;
+    
+  }
 }
+
  
   
     
