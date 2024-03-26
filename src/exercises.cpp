7a21bd2695e41c59d899765365333c944e83dd68
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-03-25 22:31:10 -0400

    preuba 22

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -161,7 +161,12 @@ void exercise_8(long int seconds) {
 
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
-
+cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+  if (s1[0] == s5[0]) {
+    return "Hemos encontrado algo!";
+  } else {
+    return "Aun sin suerte";
+  }
 }
   
 
